bea13fba17487c371e78fa8dc352824f428d4127
JesusAParedes <paredesjesus93@gmail.com>
2023-04-06 19:47:54 -0500

    Updates

---
+++ precache-manifest.81903cddeee8516aad21405ac93796da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f349bbbdc0a30e8325edcc986d199b5",
+    "url": "/411_wk5_day2_mapdispatchtoprops/index.html"
+  },
+  {
+    "revision": "a7b06fdecca1ba223af4",
+    "url": "/411_wk5_day2_mapdispatchtoprops/static/css/main.be9515fe.chunk.css"
+  },
+  {
+    "revision": "f354c04dc32cb6f89919",
+    "url": "/411_wk5_day2_mapdispatchtoprops/static/js/2.0fb69e4b.chunk.js"
+  },
+  {
+    "revision": "a7b06fdecca1ba223af4",
+    "url": "/411_wk5_day2_mapdispatchtoprops/static/js/main.c02c1cb5.chunk.js"
+  },
+  {
+    "revision": "e47b0be5e9874947a62d",
+    "url": "/411_wk5_day2_mapdispatchtoprops/static/js/runtime~main.df0822c9.js"
+  }
+]);
